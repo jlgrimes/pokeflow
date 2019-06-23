d1f2f76674542a00a61a414d2542cb54d1310076
Jared Grimes <jaregrim@amazon.com>
2019-06-23 11:04:46 -0400

    draft of rk9 finished

--- rk9/generate_rk9_graph.py
+++ rk9/generate_rk9_graph.py
@@ -1,5 +1,46 @@
 import json
 
+jsonPath = "rk9/sample_data.json"
+htmlOutPath = "rk9/out.html"
+chartWidth = 80
+chartHeight = 60
+
+htmlHead = """
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js"></script>
+<div class="chart-container" style="position: relative; height:""" + str(chartHeight) + """vh; width:""" + str(chartWidth) +  """vw">
+<canvas id="myChart" width="100" height="100"></canvas>
+</div>
+<script>
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: 
+"""
+htmlMiddle = """
+, datasets: [
+"""
+
+htmlEnd = """
+        ]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                stacked: true
+            }],
+            yAxes: [{
+                stacked: true,
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+</script>
+"""
+
 def parent(deckName):
     if "zoroark gx" in deckName.lower() and "greninja & zoroark gx" not in deckName.lower():
         return "Zoroark GX"
@@ -14,11 +55,9 @@ def parent(deckName):
     
     return ""
 
-def main():
-    with open("rk9/sample_data.json", "r") as f:
+def init(data):
+    with open(jsonPath, "r") as f:
         dataIn = json.load(f)
-
-    data = {}
 
     for deckName, deckCount in dataIn.items():
         if deckCount != 0:
@@ -29,7 +68,59 @@ def main():
             else:
                 data[deckName] = deckCount
 
-    print("hi!")
+def returnParentCount(elemValue):
+    if type(elemValue) is not dict:
+        return elemValue
+    sum = 0
+    for key, val in elemValue.items():
+        sum += val 
+    return sum
+
+def createDataSet(parentName):
+    return    """
+    {
+    label: " """ + parentName + """ ",
+    data: 
+    """
+
+def childData(elemValue, ctr):
+    htmlData = [0] * 100
+    htmlData[ctr] = elemValue
+    return htmlData
+
+def appendToDataSet(dataSet, key, val, ctr):
+    dataSet += createDataSet(key)
+    dataSet += str(childData(val, ctr))
+    dataSet += "},"
+    return dataSet
+
+def main():
+    data = {}
+    init(data)
+
+    parentLabels = []
+    parentData = []
+    dataSet = ""
+    ctr = 0
+
+    for key, val in sorted(data.items(), key=lambda x: returnParentCount(x[1]), reverse=True):
+        parentLabels.append(key)
+
+        if type(val) == int:
+            dataSet = appendToDataSet(dataSet, key, val, ctr)
+        else:
+            for childKey, childValue in val.items():
+                dataSet = appendToDataSet(dataSet, childKey, childValue, ctr)
+        # parentData.append(val)
+        ctr += 1
+        #break
+
+    dataSet = dataSet[:-1]
+    print(parentLabels)
+    print(parentData)
+
+    with open(htmlOutPath, "w+") as f:
+        f.write(htmlHead + str(parentLabels) + htmlMiddle + dataSet + htmlEnd)
 
 
 if __name__ == "__main__":
